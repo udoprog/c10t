17570b07cb0d16613b09b783f534edeaebe94b6e
John-John Tedro <udoprog@udoprog-laptop.(none)>
2010-09-23 18:20:46 +0200

    Finally found the last leak

--- src/Level.cpp
+++ src/Level.cpp
@@ -57,6 +57,8 @@ void register_byte_array(void *context, nbt::String name, nbt::ByteArray* byte_a
     level->blocklight = byte_array;
     return;
   }
+  
+  delete byte_array;
 }
 
 void error_handler(void *context, size_t where, const char *why) {

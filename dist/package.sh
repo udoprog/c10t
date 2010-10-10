a46abf467dea9b7d6fe6bed103b5d78b90ce8b58
John-John Tedro <johnjohn.tedro@gmail.com>
2010-10-10 23:58:52 +0200

    Updated to specifically set version

--- dist/package.sh
+++ dist/package.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+
+version=$2
+
 set -e
-make -f dist/Makefile.linux clean package
-make -f dist/Makefile.linux32 clean package
-make -f dist/Makefile.windows clean package
-make -f dist/Makefile.windows32 clean package
+make -f dist/Makefile.linux clean package VERSION="$version"
+make -f dist/Makefile.linux32 clean package VERSION="$version"
+make -f dist/Makefile.windows clean package VERSION="$version"
+make -f dist/Makefile.windows32 clean package VERSION="$version"
 [ -d $1 ] && cp build/*.tar.gz build/*.zip $1

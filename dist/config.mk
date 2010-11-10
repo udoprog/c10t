87addce1a7492e0057046383ec71434759ea9bb6
John-John Tedro <johnjohn.tedro@gmail.com>
2010-11-10 18:03:59 +0100

    Added non standard algorithms to project

--- dist/config.mk
+++ dist/config.mk
@@ -1,19 +1,27 @@
+SOURCES+=src/algorithm.cpp
 SOURCES+=src/level.cpp
-SOURCES+=src/color.cpp
 SOURCES+=src/blocks.cpp
 SOURCES+=src/world.cpp
 SOURCES+=src/text.cpp
 SOURCES+=src/players.cpp
 SOURCES+=src/fileutils.cpp
-SOURCES+=src/image.cpp
 SOURCES+=src/common.cpp
 SOURCES+=src/nbt/nbt.cpp
 SOURCES+=src/main.cpp
 SOURCES+=src/utf8.cpp
 SOURCES+=src/warps.cpp
+SOURCES+=src/engine/isometric_engine.cpp
+SOURCES+=src/engine/oblique_engine.cpp
+SOURCES+=src/engine/obliqueangle_engine.cpp
+SOURCES+=src/engine/topdown_engine.cpp
+SOURCES+=src/image/cached_image.cpp
+SOURCES+=src/image/color.cpp
+SOURCES+=src/image/image_base.cpp
+SOURCES+=src/image/image_operations.cpp
+SOURCES+=src/image/memory_image.cpp
 
 OBJECTS=${SOURCES:.cpp=.o}
-CXXFLAGS+=-I${USR}/include/freetype2 -Wall -fomit-frame-pointer -O2
+CXXFLAGS+=-Isrc -I${USR}/include/freetype2 -Wall -fomit-frame-pointer -O2
 
 PACKAGE=${DIST}-${VERSION}
 BUILD=./build

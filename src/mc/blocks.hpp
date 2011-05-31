3573a1d84b9c6d27c8677c0f23396960bb1fdbfd
Athemis <alexander.minges@athemis.de>
2011-05-31 12:25:17 +0200

    Typo

--- src/mc/blocks.hpp
+++ src/mc/blocks.hpp
@@ -97,10 +97,10 @@ namespace mc {
     Bloodstone = 0x57,
     Slowsand = 0x58,
     Lightstone = 0x59,
-    Trapdoor = 0x60,
     Portal = 0x5A,
     Jackolantern = 0x5B,
     Cake = 0x5C,
+    Trapdoor = 0x60,
     PineLeaves = 0xEC,
     BirchLeaves = 0xED,
     MaterialCount = 0xEE

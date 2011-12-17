0968b57770531428316d209676f5217bd42ddf62
John-John Tedro <johnjohn.tedro@gmail.com>
2011-12-17 14:10:35 +0000

    Enabled experimental caching for mcregion

--- src/main.cpp
+++ src/main.cpp
@@ -744,6 +744,7 @@ bool generate_map(settings_t &s, fs::path& world_path, fs::path& output_path) {
          
         job.level = level;
         job.coord = rl.coord;
+        job.path = rl.level->get_region()->get_path();
         
         renderer.give(job);
         ++queued;

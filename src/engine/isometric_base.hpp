e492ec13d65d26c8acf67b371e715bdb20897352
Golden-Lion <Golden-Lion@users.noreply.github.com>
2014-08-24 20:35:06 +0200

    Fixed previous commit

--- src/engine/isometric_base.hpp
+++ src/engine/isometric_base.hpp
@@ -85,7 +85,7 @@ public:
               // Check if the requested block is the top block
               if(block_data & 0x08) {
                 // Small sanity check
-                if(y > 0 && mc::MaterialModes[br_blocks.get8(y-1)] == mc::LargeFlowerBlock) {
+                if(y > 0 && br_blocks.get8(y-1) == block_type) {
                   // Minecraft currently doesn't set the lower bits to the
                   // corresponding type so we have to do this here.
                   block_data = br_data.get4(y-1) | 0x08;
